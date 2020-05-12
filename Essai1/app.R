f35b39168d665c35ff2c0359fa0bc0e972e9c749
unknown <tbarrande@etu.isara.fr>
2020-05-12 15:41:13 +0200

    1er essai

---
+++ Essai1/app.R
@@ -0,0 +1,33 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+    verbatimTextOutput("summary"),
+    tableOutput("table")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    output$summary <- renderPrint({
+        dataset <- get(input$dataset, "package:datasets")
+        summary(dataset)
+    })
+    
+    output$table <- renderTable({
+        dataset <- get(input$dataset, "package:datasets")
+        dataset
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
